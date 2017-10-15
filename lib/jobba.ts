14034f3b3bdf653a5acbc43e5f36fe578c600600
Ross Hadden <rosshadden@gmail.com>
2017-10-15 00:33:34 -0400

    Got Jobba working, with register and schedule methods.

--- lib/jobba.ts
+++ lib/jobba.ts
@@ -1,15 +1,23 @@
-interface JobbaConfig {
-	port: number;
-}
+import * as Bull from 'bull';
+
+interface JobbaConfig {}
 
 export default class Jobba {
 	config: JobbaConfig;
+	queues: Map<string, Bull.Queue>;
 
 	constructor(config) {
 		this.config = config;
+		this.queues = new Map();
 	}
 
-	start() {
-		console.log('start');
+	register(id: string, fn: (job: Bull.Job) => Promise<any>, options?: Bull.QueueOptions) {
+		const queue = new Bull(id, options);
+		this.queues.set(id, queue);
+		queue.process(fn);
+	}
+
+	schedule(id: string, data: any, options?: Bull.JobOptions) {
+		this.queues.get(id).add(data, options);
 	}
 }

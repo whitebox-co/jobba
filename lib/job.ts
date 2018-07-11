f1a436695cfc58303f018532fe695a56dab59a4c
Ross Hadden <rosshadden@gmail.com>
2018-07-11 14:36:26 -0400

    Added logging to jobs.

--- lib/job.ts
+++ lib/job.ts
@@ -1,8 +1,24 @@
 import * as Bull from 'bull';
 import { toPromise } from './utils';
 
+type LogLevel = 'debug' | 'error' | 'info' | 'warn';
+interface Log {
+	level: LogLevel;
+	time: Date;
+	body: Array<any>;
+}
+
 export default class Job {
+	private data: {
+		logs: Array<Log>;
+		state: any;
+	};
+
 	constructor(private job: Bull.Job) {
+		this.data = {
+			logs: [],
+			state: undefined,
+		};
 	}
 
 	// Proxies
@@ -13,5 +29,22 @@ export default class Job {
 	public discard() { return toPromise((this.job as any).discard()); }
 	public promote() { return toPromise(this.job.promote()); }
 	public finished() { return toPromise(this.job.finished()); }
-	public update(state?: any) { return toPromise(this.job.update(state)); }
+
+	public update(state?: any) {
+		if (arguments.length) this.data.state = state;
+		return toPromise(this.job.update(this.data));
+	}
+
+	public log(level: LogLevel, ...body) {
+		this.data.logs.push({
+			level,
+			time: new Date(),
+			body,
+		});
+		return this.update();
+	}
+	public debug(...body) { return this.log('debug', ...body); }
+	public error(...body) { return this.log('error', ...body); }
+	public info(...body) { return this.log('info', ...body); }
+	public warn(...body) { return this.log('warn', ...body); }
 }

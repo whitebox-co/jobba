5b6360bd8deb245dec1ec5ad6aaa0df976ecc1ea
Ross Hadden <rosshadden@gmail.com>
2019-09-29 12:02:08 -0400

    Added two endpoints.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -3,5 +3,13 @@ import { JobbaContext } from '../lib';
 export default {
 	Query: {
 		healthcheck: () => true,
+
+		taskIds: (parent, args, ctx: JobbaContext) => {
+			return ctx.jobba.list();
+		},
+
+		tasks: (parent, args, ctx: JobbaContext) => {
+			return ctx.jobba.tasks.values();
+		},
 	},
 };
